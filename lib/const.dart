a7d0caf63ac5c0e6a3b1f0b94bfbcc6ca996aa96
Kai He <hekai_aggie2013@163.com>
2024-09-23 15:46:29 +0800

    complete result page design and re-organize files

--- lib/const.dart
+++ lib/const.dart
@@ -7,4 +7,11 @@ const inactiveCardColor = Color(0xFF111328);
 const labelTextStyle = TextStyle(fontSize: 18, color: Color(0xFF8D8E98));
 const numberTextStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.w900);
 const calculateButtonTextStyle =
-    TextStyle(fontSize: 25, fontWeight: FontWeight.w900);
+    TextStyle(fontSize: 25, fontWeight: FontWeight.bold);
+const resultTitleTextStyle =
+    TextStyle(fontSize: 50, fontWeight: FontWeight.bold);
+const resultTextStyle = TextStyle(
+    fontSize: 22, color: Color(0xFF24D876), fontWeight: FontWeight.bold);
+const resultNumberTextStyle =
+    TextStyle(fontSize: 100, fontWeight: FontWeight.bold);
+const resultInterpretationTextStyle = TextStyle(fontSize: 22);
